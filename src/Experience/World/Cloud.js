0498c0ed4b64c70571e3c7ccc7dc0a240738f76a
Brian <brian.kyounghoon.kim@gmail.com>
2022-07-25 13:27:43 -0400

    cloud animation randomize

--- src/Experience/World/Cloud.js
+++ src/Experience/World/Cloud.js
@@ -9,6 +9,16 @@ export default class marioLand {
     this.debug = this.experience.debug;
     this.time = this.experience.time;
 
+    const getRandNumb = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.random() * (max - min + 1) + min;
+    };
+
+    this.randNum = (getRandNumb(1, 3.5) / 10).toFixed(2);
+
+    console.log(this.randNum);
+
     this.setModel();
   }
 
@@ -35,12 +45,59 @@ export default class marioLand {
   }
 
   update() {
-    // this.model.geometry.traverse((child) => {
-    //   let randomNumb = Math.random();
-    //   if (child instanceof THREE.Mesh) {
-    //     child.position.x = Math.sin(this.time.elapsed * 0.0005) * 0.75 - 4;
-    //   }
-    // });
-    this.model.geometry.position.x = Math.sin(this.time.elapsed * 0.0005) * 0.5;
+    this.model.geometry.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        if (child.name.includes("001")) {
+          child.position.x =
+            Math.sin(this.time.elapsed * 0.0005) * this.randNum -
+            2.998856544494629;
+          return;
+        }
+        if (child.name.includes("002")) {
+          child.position.x =
+            Math.cos(this.time.elapsed * 0.0005) * 0.15 - 3.1958062648773193;
+          // console.log(child.position.x);
+        }
+        if (child.name.includes("003")) {
+          child.position.x =
+            Math.sin(this.time.elapsed * 0.0005) * this.randNum -
+            3.2886857986450195;
+        }
+        if (child.name.includes("004")) {
+          child.position.x =
+            Math.cos(this.time.elapsed * 0.0005) * this.randNum -
+            3.116790294647217;
+          // console.log(child.position.x);
+        }
+        if (child.name.includes("005")) {
+          child.position.x =
+            Math.cos(this.time.elapsed * 0.0005) * this.randNum -
+            3.340785503387451;
+        }
+        if (child.name.includes("006")) {
+          child.position.x =
+            Math.sin(this.time.elapsed * 0.0005) * this.randNum -
+            3.724377393722534;
+          // console.log(child.position.x);
+        }
+        if (child.name.includes("007")) {
+          child.position.x =
+            Math.cos(this.time.elapsed * 0.0005) * this.randNum -
+            3.9110183715820312;
+          // console.log(child.position.x);
+        }
+        if (child.name.includes("008")) {
+          child.position.x =
+            Math.sin(this.time.elapsed * 0.0005) * this.randNum -
+            4.181217193603516;
+          // console.log(child.position.x);
+        }
+        if (child.name.includes("009")) {
+          child.position.x =
+            Math.cos(this.time.elapsed * 0.0005) * this.randNum -
+            4.342790126800537;
+        }
+      }
+    });
   }
 }
